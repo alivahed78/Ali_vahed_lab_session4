129f83a03eb95a9cd60735b08cc11a266e13b086
alivahed78 <52452898+alivahed78@users.noreply.github.com>
2020-10-25 18:08:09 +0330

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package main;
+
+/**
+ *
+ * @author user
+ */
+public class Person {
+    String firstname , lastname;
+    
+    public Person(String firstname , String lastname){
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+    
+    public String getFirstname(){
+        return firstname;
+        
+    }
+    
+    public String getLastname(){
+        return lastname;
+        
+    }
+    
+    public String toString(){
+        lastname.toString();
+        firstname.toString();
+        return null;
+        
+    }
+}
